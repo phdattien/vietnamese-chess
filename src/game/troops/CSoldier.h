e72f3f32779ab68104ea1d289d386862c4bc0c36
Tien Dat Pham <ph.dattien@gmail.com>
2022-05-13 21:26:46 +0200

    Fix encapsulation

--- src/game/troops/CSoldier.h
+++ src/game/troops/CSoldier.h
@@ -10,11 +10,10 @@
 class CSoldier : public CTroop {
 public:
     CSoldier ( const std::string &name, SIDE side, const CCoord &coord );
-
-private:
-public:
     std::set<CCoord> getPossibleMoves ( const std::unique_ptr<CTroop> (*currBoard)[8] ) const override;
+private:
     static const std::vector<int> POS_COL;
     static const std::vector<int> POS_ROW;
+    bool isValidCoord ( const CCoord &newCoord, const std::unique_ptr<CTroop> (*currBoard)[8] ) const;
 };
 
