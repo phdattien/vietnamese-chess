cc775626a4750bfbc9009f99d3819d6eaa54f659
Tien Dat Pham <ph.dattien@gmail.com>
2022-05-22 12:58:24 +0200

    Add Move functions and generate movements

--- src/game/CBoard.cpp
+++ src/game/CBoard.cpp
@@ -5,6 +5,7 @@
 #include "CBoard.h"
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 
 
 
@@ -13,14 +14,12 @@ CBoard::CBoard (  std::vector<std::shared_ptr<CTroop>> &troops ) {
         if ( troop->getSide() == SIDE::RED ) {
             if ( troop->getName() == GENERAL )
                 m_RedGeneral = troop;
-            else
-                m_RedTroops.push_back (troop);
+            m_RedTroops.push_back (troop);
         }
         else {
             if ( troop->getName() == GENERAL )
                 m_BlackGeneral = troop;
-            else
-                m_BlackTroops.push_back (troop);
+            m_BlackTroops.push_back (troop);
         }
 
         m_Board[troop->getCoord().m_Colum][troop->getCoord().m_Row] = move ( troop ) ;
@@ -32,7 +31,7 @@ bool CBoard::isLegalMove ( const std::shared_ptr<CTroop> &troopOnPos, const CCoo
     return troopOnPos->getPossibleMoves ( m_Board ).count (dest);
 }
 
-void CBoard::print () const {
+void CBoard::printBoard () const {
     // --------------- printing header  -----------------------
     char c = 'A';
     for ( size_t i = 0; i < ROW_SIZE; i++ ) {
@@ -48,7 +47,7 @@ void CBoard::print () const {
     const char * dash = "----";
     const char * space = " ";
     for ( size_t i = 0; i < COL_SIZE; i++ ) {
-        // print left side
+        // printBoard left side
         printf ( "%2d", num--);
         printf ( "%2s", space);
 
@@ -81,26 +80,16 @@ void CBoard::print () const {
     printf("\n");
 }
 
-const std::shared_ptr<CTroop> &CBoard::findKing () const {
-    int kingCol = 0;
-    int kingRow = 0;
-    for (auto & i : m_Board) {
-        for (auto & j : i) {
-            if ( j->getName() == "K") { // find da black general
-                return j;
-            }
-        }
-    }
-    return m_Board[kingRow][kingCol];
-}
 
-bool CBoard::isGeneralsFace ( ) {
-    auto & King = findKing();
-    size_t i = King->getCoord().m_Colum + 1;
-    for ( ; i < COL_SIZE; i++ ) {
-        auto & newCord =  m_Board[i][King->getCoord().m_Row];
-        if (  newCord && newCord->getName() != "K" )
+bool CBoard::isGeneralsFacing ( ) {
+    if ( m_BlackGeneral->getCoord().m_Row != m_RedGeneral->getCoord().m_Row)
+        return false;
+
+    for ( int i = m_BlackGeneral->getCoord().m_Colum + 1; i < m_RedGeneral->getCoord().m_Colum; i++ ) {
+        auto & troopOnPos =  m_Board[i][m_RedGeneral->getCoord().m_Row];
+        if (  troopOnPos ) {
             return false;
+        }
     }
     return true;
 }
@@ -113,10 +102,130 @@ std::shared_ptr<CTroop> CBoard::getTroopOnCoord ( const CCoord &from ) const {
     return m_Board[from.m_Colum][from.m_Row];
 }
 
-void CBoard::Move ( const CCoord &from, const CCoord &to )  {
-    auto & troopOnPos =  m_Board[from.m_Colum][from.m_Row];
-    troopOnPos->setCoord (to); // set new coordinates
-    m_Board[to.m_Colum][to.m_Row] = move ( troopOnPos );
+//void CBoard::Move ( const CCoord &from, const CCoord &to )  {
+//    auto & troopOnPos =  m_Board[from.m_Colum][from.m_Row];
+//    troopOnPos->setCoord (to); // set new coordinates
+//    m_Board[to.m_Colum][to.m_Row] = move ( troopOnPos );
+//
+//}
 
+std::vector<std::shared_ptr<CTroop>> &CBoard::getTroopsOnPlay ()  {
+    return RedToMove ? m_RedTroops : m_BlackTroops;
 }
+
+std::vector<Move> CBoard::generatePseudoLegalMoves () {
+    std::vector<Move> moves;
+    std::vector<std::shared_ptr<CTroop>> troops = getTroopsOnPlay();
+    for ( const auto & troop : troops ) {
+        addMoves ( troop->getCoord(), troop->getPossibleMoves (m_Board), moves );
+    }
+    return moves;
+}
+
+void CBoard::addMoves ( const CCoord & start, const std::set<CCoord>& cords, std::vector<Move>& moves ) {
+    for ( const auto & coord : cords ) {
+        moves.emplace_back(start, coord);
+    }
+}
+
+void CBoard::printAttackedMoves () {
+    std::vector<Move> moves = generatePseudoLegalMoves();
+    for ( const auto [from, to] : moves ) {
+        printf ( "from: %d %d to: %d %d\n", from.m_Colum, from.m_Row, to.m_Colum, to.m_Row );
+    }
+}
+
+void CBoard::printAttackedMap () {
+//    std::vector<Move> moves = generatePseudoLegalMoves();
+    std::vector<Move> moves = generateMoves();
+    std::vector<CCoord> cords;
+
+    cords.reserve(moves.size());
+    for ( const auto [from, to] : moves ) {
+        cords.push_back (to);
+    }
+
+    for ( size_t i = 0; i < COL_SIZE; i++ ) {
+        for ( size_t j = 0; j < ROW_SIZE; j++ ) {
+            printf (" ");
+            if ( std::find(cords.begin(), cords.end(), CCoord(i, j)) != cords.end() )
+                printf ( "X");
+            else
+                printf ( ".");
+        }
+        printf ( "\n");
+    }
+}
+
+void CBoard::MakeMove ( const Move &movement ) {
+    RedToMove = !RedToMove; // after making movements switch sides
+    std::vector<std::shared_ptr<CTroop>> &troops = getTroopsOnPlay();
+//    std::vector<std::shared_ptr<CTroop>> troops = m_BlackTroops;
+    CCoord from = movement.m_From;
+    CCoord to = movement.m_To;
+
+    auto & troopOnPos = getTroopOnCoord (from);
+    m_PrevCapturedTroop = getTroopOnCoord (to); // can be null
+    auto it = std::find(troops.begin(), troops.end(), m_PrevCapturedTroop);
+
+    if ( it != troops.end() ) {
+        troops.erase ( it );
+    }
+
+    troopOnPos->setCoord (to); // set new coordinates
+
+    // move troop to desired destination
+    m_Board[to.m_Colum][to.m_Row] = move ( troopOnPos );
+}
+
+// can only used after MakeMove
+void CBoard::UnMakeMove ( const Move &movement ) {
+    std::vector<std::shared_ptr<CTroop>> troops = getTroopsOnPlay();
+    RedToMove = !RedToMove; // after making movements switch sides
+    CCoord from = movement.m_From;
+    CCoord to = movement.m_To;
+    auto & troopOnPos = getTroopOnCoord (to);
+//    m_PrevCapturedTroop = getTroopOnCoord (to); // can be null
+
+    troopOnPos->setCoord (from); // set where he came from
+    // move troop to desired destination
+    m_Board[from.m_Colum][from.m_Row] = move ( troopOnPos );
+    m_Board[to.m_Colum][to.m_Row] = m_PrevCapturedTroop;
+    troops.push_back (m_PrevCapturedTroop);
+}
+
+const std::vector<Move> &CBoard::generateMoves () {
+    possibleMoves.clear();
+    std::vector<Move> pseudoLegalMoves = generatePseudoLegalMoves();
+
+    for ( auto & possibleMov : pseudoLegalMoves ) {
+        MakeMove (possibleMov);
+        std::vector<Move> opponentMoves = generatePseudoLegalMoves();
+
+        CCoord generalCoord = getGeneralOnOppositePlayCoord();
+        auto res = std::find_if(opponentMoves.begin(), opponentMoves.end(), [&generalCoord]  (const Move & a ) {
+            return a.m_To == generalCoord;
+        });
+
+        std::cout << "General COord: " << generalCoord.m_Colum << " " << generalCoord.m_Row << std::endl;
+
+        if ( res == opponentMoves.end() &&  ! isGeneralsFacing() ) {
+            possibleMoves.push_back (possibleMov);
+        }
+        UnMakeMove (possibleMov);
+    }
+
+    return possibleMoves;
+}
+
+const CCoord &CBoard::getGeneralOnPlayCoord () const {
+    return RedToMove ? m_RedGeneral->getCoord() : m_BlackGeneral->getCoord();
+}
+
+const CCoord &CBoard::getGeneralOnOppositePlayCoord () const {
+    return RedToMove ? m_BlackGeneral->getCoord() : m_RedGeneral->getCoord();
+}
+
+
+
 
--- src/game/CBoard.h
+++ src/game/CBoard.h
@@ -2,6 +2,7 @@
 #include "troops/CTroop.h"
 #include <vector>
 #include <memory>
+#include "Move.h"
 
 const size_t ROW_SIZE = 9;
 const size_t COL_SIZE = 10;
@@ -16,23 +17,49 @@ public:
     CBoard () = default;
 
     bool isLegalMove ( const std::shared_ptr<CTroop> & troopOnPos, const CCoord & dest ) const;
-    // find if two generals face each other return false
-    bool isGeneralsFace   ();
-    std::shared_ptr<CTroop> & getTroopOnCoord ( const CCoord & from );
-    std::shared_ptr<CTroop> getTroopOnCoord ( const CCoord & from ) const;
-    void Move ( const CCoord &from, const CCoord & to);
-    void print () const;
+    // return true if two generals are facing each other
+    bool isGeneralsFacing   ();
 
+    std::shared_ptr<CTroop> & getTroopOnCoord ( const CCoord & from );
+
+    std::shared_ptr<CTroop> getTroopOnCoord ( const CCoord & from ) const;
+
+    std::vector<std::shared_ptr<CTroop>> & getTroopsOnPlay ();
+
+    // void Move ( const CCoord &from, const CCoord & to);
+
+    void printBoard () const;
+
+    //getAllpseudoLegalMoves
+    std::vector<Move> generatePseudoLegalMoves ();
+
+    void printAttackedMoves ();
+
+    void printAttackedMap ();
+
+    void addMoves ( const CCoord & start, const std::set<CCoord>& cords, std::vector<Move>& moves );
+
+    void MakeMove ( const Move & movement );
+
+    const CCoord & getGeneralOnPlayCoord () const;
+
+    const CCoord & getGeneralOnOppositePlayCoord () const;
+
+    void UnMakeMove ( const Move & movement );
+
+
+    const std::vector<Move> & generateMoves ();
 
 private:
+    std::string GENERAL = "G";
+    std::shared_ptr<CTroop> m_Board[COL_SIZE][ROW_SIZE];
     std::vector<std::shared_ptr<CTroop>> m_BlackTroops;
     std::vector<std::shared_ptr<CTroop>> m_RedTroops;
     std::shared_ptr<CTroop> m_RedGeneral;
     std::shared_ptr<CTroop> m_BlackGeneral;
+    std::shared_ptr<CTroop> m_PrevCapturedTroop;
+    std::vector<Move> possibleMoves;
     // who is moving right now
     bool RedToMove = true;
-    std::shared_ptr<CTroop> m_Board[COL_SIZE][ROW_SIZE];
-    const std::shared_ptr<CTroop> & findKing ( ) const;
-    std::string GENERAL = "G";
 };
 
