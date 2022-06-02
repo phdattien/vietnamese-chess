16bbb4e651d751b606ee45201febb5b2679cabc7
Tien Dat Pham <ph.dattien@gmail.com>
2022-06-02 05:07:11 +0200

    Add Perft testing

--- tests/Perft.cpp
+++ tests/Perft.cpp
@@ -4,7 +4,6 @@
 
 #include "Perft.h"
 #include <vector>
-#include <iostream>
 
 Perft::Perft ( CBoard board ) : m_Board ( std::move (board) ) {}
 
@@ -22,22 +21,3 @@ int Perft::movesTest ( int depth ) {
     return nodes;
 }
 
-//u64 Perft(int depth)
-//{
-//    MOVE move_list[256];
-//    int n_moves, i;
-//    u64 nodes = 0;
-//
-//    if (depth == 0)
-//        return 1ULL;
-//
-//    n_moves = GenerateMoves(move_list);
-//    for (i = 0; i < n_moves; i++) {
-//        MakeMove(move_list[i]);
-//        if (!IsIncheck())
-//            nodes += Perft(depth - 1);
-//        UndoMove(move_list[i]);
-//    }
-//    return nodes;
-//}
-//
